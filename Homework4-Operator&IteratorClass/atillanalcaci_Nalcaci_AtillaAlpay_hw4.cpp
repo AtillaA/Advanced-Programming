a2efe32cb503fc92b509156038cdf8fc0a852d26
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2019-10-21 16:48:52 +0300

    Update atillanalcaci_Nalcaci_AtillaAlpay_hw4.cpp

--- Homework4-Operator&IteratorClass/atillanalcaci_Nalcaci_AtillaAlpay_hw4.cpp
+++ Homework4-Operator&IteratorClass/atillanalcaci_Nalcaci_AtillaAlpay_hw4.cpp
@@ -19,147 +19,133 @@ int main()
 	linkedlist a;
 	linkedlist b;
 	string x;
+	
 	cout << "Enter lesson name: ";
-	cin>>x;
-	//bool check=true;
+	cin >> x;
+	
 	string sID;
 	string add;
+	
 	cout << "Enter student ID: " ;
-	cin>> sID;
-	while(sID!="^Z")
-	{
-		/*if(sID=="^Z")
-		{
-			check=false;
-		}*/
-		if(add.find(sID)==string::npos)
-		{
-		a.addSorted(sID);
-		add+=".."+sID;
+	cin >> sID;
+	
+	while(sID!="^Z") {
+		if(add.find(sID) == string::npos) {
+			a.addSorted(sID);
+			add += ".." + sID;
 		}
-		else
-		{
+		
+		else {
 			cout << "Already in the list." << endl;
 		}
+		
 		cout << "Enter student ID: " ;
-		cin>> sID;
-		
-		
+		cin >> sID;
 	}
-	cout <<"__LIST1__" << endl;
+	
+	cout << "__LIST1__" << endl;
 	a.printList();
-	//cin.clear();
+	
 	string x2;
 	cout << "Enter lesson name: ";
-	cin>>x2;
-	//bool check2=true;
+	cin >> x2;
+	
 	string sID2;
-	string added="";
+	string added = "";
+	
 	cout << "Enter student ID: " ;
-	cin>> sID2;
-	while(sID2!="^Z")
-	{
-		/*if(cin.eof())
-		{
-			check2=false;
-		}*/
-		if(added.find(sID2)==string::npos)
-		{
-		b.addSorted(sID2);
-		added+=".." + sID2;
+	cin >> sID2;
+	
+	while(sID2 != "^Z") {
+		if(added.find(sID2)==string::npos) {
+			b.addSorted(sID2);
+			added += ".." + sID2;
+		}
 		
-		}
-		else
-		{
-			cout << "already in the list." << endl;
-		}
+		else { cout << "already in the list." << endl; }
+		
 		cout << "Enter student ID: " ;
-		cin>> sID2;
+		cin >> sID2;
 	}
+	
 	cout <<"__LIST2__" << endl;
 	b.printList();
 	cin.clear();
 	Options();
+	
 	int select;
-	cin>> select;
+	cin >> select;
 	cout << "**" << endl;
-	while(select!=5)
-	{
-		if(select==1)
-		{
+	
+	while(select!=5) {
+		if(select==1) {
 			linkedlist result1;
-		    result1=a+b;
+		    	result1 = a + b;
 			result1.printList();
 		}
-		if(select==2)
-		{
+		
+		if(select==2) {
 			int list;
 			cout << "Enter lefthandside list: " << endl;
-			cin>>list;
+			cin >> list;
 			cout << "selected." << endl;
 			
-			if(list==1)
-			{
+			if(list==1) {
 				linkedlist result2_1;
-				result2_1=a-b;
+				result2_1 = a - b;
 				result2_1.printList();
 			}
+			
 			else
 			{
 				if (list==2)
 				{
 					linkedlist result2_2;
-					result2_2=b-a;
+					result2_2 = b - a;
 					result2_2.printList();
 				}
-				else
-				{
-					cout << "This is not an option." << endl;
-				}
+				
+				else { cout << "This is not an option." << endl; }
 			}
 		}
-		if(select==3)
-		{
+		
+		if(select == 3) {
 			linkedlist result3;
-		    result3=a&b;
+		    	result3 = a & b;
 			result3.printList();
 		}
-		if(select==4)
-		{
+		
+		if(select == 4) {
 			int ll;
 			cout << "Select list: ";
 			cin >> ll;
 			cout << "selected." << endl;
-			if(ll==1)
-			{
+			
+			if(ll == 1) {
 				int index;
 				cout << "Enter index:" ;
-				cin>> index;
+				cin >> index;
 				cout << "Index " << index <<" of the list: " << a[index] << endl;
 			}
-			else
-			{
-				if(ll==2)
-				{
+			
+			else {
+				if(ll == 2) {
 					int index2;
 					cout << "Enter index:" ;
-					cin>> index2;
+					cin >> index2;
 					cout << "Index " << index2 <<" of the list: " << b[index2] << endl;
 				}
-				else 
-				{
+				
+				else {
 					cout <<"Not an option." << endl;
 				}
 			}
-
 		}
-		if(select!=1&&select!=3&&select!=4&&select!=2)
-		{
-			cout << "Invalid option selected"<<endl;
-		}
+		
+		if(select != 1 && select != 3 && select != 4 && select !=2 ) { cout << "Invalid option selected"<<endl; }
 		Options();
-		cin>> select;
-		cout <<"**" << endl;
+		cin >> select;
+		cout << "**" << endl;
 	}
 	
 	return 0;
